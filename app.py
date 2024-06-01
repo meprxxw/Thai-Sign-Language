bbb293533347e12a8fe319569f85a1c76725c581
feat.praew <123751517+meprxxw@users.noreply.github.com>
2024-06-01 20:36:50 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,174 @@
 import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
- 'first column': [1, 2, 3, 4],
- 'second column': [10, 20, 30, 40]
-})
-st.write("Here's our first table:")
-st.write(df)
-
-import streamlit as st
+import mediapipe as mp
+import cv2
 import numpy as np
-import pandas as pd
-chart_data = pd.DataFrame(
- np.random.randn(20, 3),
- columns=['a', 'b', 'c'])
-st.line_chart(chart_data)
+import tempfile
+import time
+from PIL import Image, ImageDraw, ImageFont
+import os
+import time
+from streamlit_webrtc import VideoProcessorBase, RTCConfiguration,WebRtcMode,webrtc_streamer
+from utils import *
+import cv2
+# import avs
+from PIL import ImageFont, Image, ImageDraw
+import numpy as np
+import tensorflow as tf
+import pandas as pd 
+import numpy as np
+import json
 
-map_data = pd.DataFrame(
- np.random.randn(1000, 2) / [50, 50] + [13.76514, 100.53829],
- columns=['lat', 'lon'])
-st.map(map_data)
+'''
+
+        Real-Time Version
+
+'''
+
+
+# RTC_CONFIGURATION = RTCConfiguration(
+#     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+# )
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+ROWS_PER_FRAME = 543  # number of landmarks per frame
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # color conversion
+    image.flags.writeable = False # img no longer writeable
+    pred = model.process(image) # make landmark prediction
+    image.flags.writeable = True  # img now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # color reconversion
+    return image, pred
+
+def draw(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                              mp_drawing.DrawingSpec(color=(0,0,255), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0,0,0), thickness=1, circle_radius=0))
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(0,150,0), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(200,56,12), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(250,56,12), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2))
+    
+def extract_coordinates(results):
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]) if results.face_landmarks else np.zeros((468, 3))
+    pose = np.array([[res.x, res.y, res.z] for res in results.pose_landmarks.landmark]) if results.pose_landmarks else np.zeros((33, 3))
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]) if results.left_hand_landmarks else np.zeros((21, 3))
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]) if results.right_hand_landmarks else np.zeros((21, 3))
+    return np.concatenate([face, lh, pose, rh])
+
+def load_json_file(json_path):
+    with open(json_path, 'r') as f:
+        sign_map = json.load(f)
+    return sign_map
+
+def draw_text_on_image(image, text, position, font, color=(0, 255, 0)):
+    # Convert the OpenCV image (BGR) to a PIL image (RGB)
+    image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(image_pil)
+    draw.text(position, text, font=font, fill=color)
+    # Convert the PIL image back to an OpenCV image
+    image_cv = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
+    return image_cv
+
+class CFG:
+    data_dir = ""
+    sequence_length = 12
+    rows_per_frame = 543
+
+ROWS_PER_FRAME = 543
+def load_relevant_data_subset(pq_path):
+    data_columns = ['x', 'y', 'z']
+    data = pd.read_parquet(pq_path, columns=data_columns)
+    n_frames = int(len(data) / ROWS_PER_FRAME)
+    data = data.values.reshape(n_frames, ROWS_PER_FRAME, len(data_columns))
+    return data.astype(np.float32)
+
+train = pd.read_csv('train.csv')
+
+# Add ordinally Encoded Sign (assign number to each sign name)
+train['sign_ord'] = train['sign'].astype('category').cat.codes
+
+# Dictionaries to translate sign <-> ordinal encoded sign
+SIGN2ORD = train[['sign', 'sign_ord']].set_index('sign').squeeze().to_dict()
+ORD2SIGN = train[['sign_ord', 'sign']].set_index('sign_ord').squeeze().to_dict()
+
+#actions = np.array(['สวัสดี', 'ตก', 'รอ', 'กลับ', 'ขอบคุณ', 'ตัด', 'ลง', 'ขึ้น', 'เฝ้า','คุย', 'ช่วย', 'เชื่อ', 'ฟัง', 'มอง', 'พา', 'ชวน หลีกภัย', 'ทักษิณ ชินวัตร', 'ขนม', 'พิธา ลิ้มเจริญรัตน์', 'ศิริกัญญา ตันสกุล','แบก', 'อนุทิน ชาญวีรกูล', 'รังสิมันต์ โรม', 'พีระพันธุ์ สาลีรัฐวิภาค', 'คุณหญิงสุดารัตน์ เกยุราพันธุ์', 'พลเอกอนุพงษ์ เผ่าจินดา', 'สุวัจน์ ลิปตพัลลภ', 'กรณ์ จาติกวณิช', 'วราวุธ ศิลปะอาชา', 'พล.ต.อ.เสรีพิศุทธ์ เตมียเวสวราวุธ ศิลปะอาชา', 'ศักดิ์สยาม ชิดชอบ', 'ชาดา ไทยเศรษฐ์', 'สุชัชวีร์ สุวรรณสวัสดิ์', 'จุรินทร์ ลักษณวิศิษฏ์', 'ไตรรงค์ สุวรรณคีรี', 'พลเอกประยุทธ์ จันทร์โอชา', 'นฤมล ภิญโญสินวัฒน์', 'ธรรมนัส พรหมเผ่า', 'ชัยวุฒิ ธนาคมานุสรณ์', 'ไพบูลย์ นิติตะวัน', 'พลเอกประวิตร วงษ์สุวรรณ', 'พริษฐ์ วัชรสินธุ', 'ยิ่งลักษณ์ ชินวัตร', 'ณัฐวุฒิ ใสยเกื้อ', 'นพ.ชลน่าน ศรีแก้ว', 'เศรษฐา ทวีสิน', 'แพทองธาร ชินวัตร', 'อะไร', 'สมัคร', 'กระโดด', 'ยก', 'ชน', 'ผ่าน', 'แนะนำ', 'จำได้', 'ปลูก', 'รวม', 'ทำหาย', 'เจอ', 'หาย', 'พนักงานขาย', 'นักเขียนโปรแกรม', 'พ่อครัว', 'เจ้าหน้าที่ตำรวจ', 'เนื้อลูกแกะ', 'เนื้อหมู', 'คุณสบายดีไหม', 'ดูเหมือน', 'วาง', 'อยู่', 'ก', 'ข', 'ค', 'ฆ', 'ต', 'ถ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ล', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ'])
+
+def real_time_asl():
+    interpreter = tf.lite.Interpreter("model-withflip.tflite")
+    found_signatures = list(interpreter.get_signature_list().keys())
+    prediction_fn = interpreter.get_signature_runner("serving_default")
+
+    sequence_data = []
+    cap = cv2.VideoCapture(0)
+
+    last_prediction = None
+    last_prediction_time = 0
+    prediction_display_duration = 2  # seconds
+
+    video_placeholder = st.empty()            
+
+    font_path = "./angsana.ttc"
+    font_size = 32
+    font = ImageFont.truetype(font_path, font_size)
+
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            
+            image, results = mediapipe_detection(frame, holistic)
+            draw(image, results)
+
+             # Create a placeholder for the video feed
+            landmarks = extract_coordinates(results)
+            sequence_data.append(landmarks)
+
+            if len(sequence_data) % 30 == 0:
+                prediction = prediction_fn(inputs=np.array(sequence_data, dtype = np.float32))
+                sign = np.argmax(prediction["outputs"])
+
+                st.write("Predicted Index:", sign)
+                st.write("Predicted Sign:", ORD2SIGN[sign])
+
+                last_prediction = ORD2SIGN[sign]
+                last_prediction_time = time.time()
+                sequence_data = []
+                #print(prediction)
+            current_time = time.time()
+            if last_prediction and (current_time - last_prediction_time < prediction_display_duration):
+                draw_text_on_image(image, f"Prediction:    {last_prediction}", (3, 30),
+                            font, (0, 255, 0))
+                
+            # Display the frame using Streamlit
+            video_placeholder.image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), channels="RGB")
+
+            if cv2.waitKey(10) & 0xFF == ord("q"):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+def main():
+    st.header("Live stream processing")
+
+    sign_language_det = "Sign Language Live Detector"
+    app_mode = st.sidebar.selectbox( "Choose the app mode",
+        [
+            sign_language_det
+        ],
+    )
+
+    st.subheader(app_mode)
+    if app_mode == sign_language_det:
+        if st.button("Start TSL Detection"):
+            real_time_asl()
+
+if __name__ == "__main__":
+    main()
